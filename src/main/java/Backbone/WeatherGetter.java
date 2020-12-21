703260e90dba61d66cbd9386acd2dc3879f218ff
alexanderFH <alexander.ressl@stud.fh-campuswien.ac.at>
2020-12-21 16:22:28 +0100

    api hotfix

--- src/main/java/Backbone/WeatherGetter.java
+++ src/main/java/Backbone/WeatherGetter.java
@@ -20,7 +20,9 @@ import java.util.Locale;
 public class WeatherGetter {
     public static void main(String[] args) throws IOException {
         // ArrayList<Backbone.Day> days = getWeatherJson("33.74,-84.39");
-        getCurrentWeather("1220");
+        System.out.println(getCurrentWeather("1220").getCurrentTemp());
+        ArrayList<Day> days = getWeatherJson("1220");
+        System.out.println(days.get(0));
        /* Day day = new Day("sonntat", 5, 10, "toll");//test
         System.out.println(day.getCurrentTemp());
         ArrayList<Day> days = getWeatherJson("1220:AT");
@@ -41,7 +43,7 @@ public class WeatherGetter {
     public static ArrayList<Day> getWeatherJson(String plz) throws IOException {
         ArrayList<Day> back = new ArrayList<>();
         // InputStream is = new URL("https://api.weather.com/v3/wx/forecast/daily/5day?geocode=" + coordinates + "&format=json&units=m&language=de-DE&apiKey=1531e846099f413eb1e846099ff13ef6").openStream();
-        InputStream is = new URL("https://api.weather.com/v3/wx/forecast/daily/5day?postalKey=" + plz + "&format=json&units=m&language=de-DE&apiKey=1531e846099f413eb1e846099ff13ef6").openStream();
+        InputStream is = new URL("https://api.weather.com/v3/wx/forecast/daily/5day?postalKey=" + plz + ":AT&format=json&units=m&language=de-DE&apiKey=1531e846099f413eb1e846099ff13ef6").openStream();
 
         try {
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));

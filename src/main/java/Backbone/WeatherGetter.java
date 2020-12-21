7643cac169cd44299824424a87adb9a4815143b1
Alexander Ressl <alexander.ressl@stud.fh-campuswien.ac.at>
2020-12-21 15:41:18 +0100

    new Api testing

--- src/main/java/Backbone/WeatherGetter.java
+++ src/main/java/Backbone/WeatherGetter.java
@@ -68,4 +68,24 @@ public class WeatherGetter {
         }
         return back;
     }
+
+    protected static void getCurrentWeather(String plz) throws IOException {
+        InputStream is = new URL("http://api.openweathermap.org/data/2.5/weather?zip=" + plz + ",AT&appid=6b5717bc865ffcb87230cfbcf6263078&units=metric").openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonString = readAll(rd);
+            //String jsonString = "{\"coord\":{\"lon\":16.5,\"lat\":48.22},\"weather\":[{\"id\":803,\"main\":\"Clouds\",\"description\":\"broken clouds\",\"icon\":\"04n\"}],\"base\":\"stations\",\"main\":{\"temp\":2.97,\"feels_like\":0.59,\"temp_min\":1.67,\"temp_max\":3.89,\"pressure\":1026,\"humidity\":93},\"visibility\":10000,\"wind\":{\"speed\":1,\"deg\":160},\"clouds\":{\"all\":75},\"dt\":1608222958,\"sys\":{\"type\":1,\"id\":6878,\"country\":\"AT\",\"sunrise\":1608187190,\"sunset\":1608217246},\"timezone\":3600,\"id\":0,\"name\":\"Wien, Donaustadt\",\"cod\":200}\n"
+            System.out.println(jsonString);
+            JSONObject obj = new JSONObject(jsonString);
+            JSONObject weather = obj.getJSONArray("weather").getJSONObject(0);
+            JSONObject temp = obj.getJSONObject("main");
+            System.out.println(temp);
+            System.out.println(weather.get("main")); // Jetziges Wetter Sonne/Wolke/whatever
+            //System.out.println(weather.get(0).);
+        } catch (Exception e) {
+
+        } finally {
+            is.close();
+        }
+    }
 }

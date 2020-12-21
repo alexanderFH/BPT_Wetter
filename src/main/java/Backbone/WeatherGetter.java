6dc2874cf0e896178be53e37382d42ff3bf73293
alexanderFH <alexander.ressl@stud.fh-campuswien.ac.at>
2020-12-21 17:05:44 +0100

    catch exceptions in backbone

--- src/main/java/Backbone/WeatherGetter.java
+++ src/main/java/Backbone/WeatherGetter.java
@@ -5,6 +5,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -20,9 +21,10 @@ import java.util.Locale;
 public class WeatherGetter {
     public static void main(String[] args) throws IOException {
         // ArrayList<Backbone.Day> days = getWeatherJson("33.74,-84.39");
-        System.out.println(getCurrentWeather("1220", "AT").getCurrentTemp());
-        ArrayList<Day> days = getWeatherJson("1220", "AT");
-        System.out.println(days.get(0));
+        getWeatherJson("8872","CH");
+        // System.out.println(getCurrentWeather("1220", "AT").getNarrative());
+        // ArrayList<Day> days = getWeatherJson("1220", "AT");
+        // System.out.println(days.get(0));
        /* Day day = new Day("sonntat", 5, 10, "toll");//test
         System.out.println(day.getCurrentTemp());
         ArrayList<Day> days = getWeatherJson("1220:AT");
@@ -40,67 +42,58 @@ public class WeatherGetter {
         return sb.toString();
     }
 
-    public static ArrayList<Day> getWeatherJson(String plz, String country) throws IOException {
+    public static ArrayList<Day> getWeatherJson(String plz, String country) {
         ArrayList<Day> back = new ArrayList<>();
-        // InputStream is = new URL("https://api.weather.com/v3/wx/forecast/daily/5day?geocode=" + coordinates + "&format=json&units=m&language=de-DE&apiKey=1531e846099f413eb1e846099ff13ef6").openStream();
-        InputStream is = new URL("https://api.weather.com/v3/wx/forecast/daily/5day?postalKey=" + plz + ":" + country + "&format=json&units=m&language=de-DE&apiKey=1531e846099f413eb1e846099ff13ef6").openStream();
-
-        try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+        back.add(getCurrentWeather(plz, country));
+        try (InputStream is = new URL("https://api.weather.com/v3/wx/forecast/daily/5day?postalKey=" + plz + ":" + country + "&format=json&units=m&language=de-DE&apiKey=1531e846099f413eb1e846099ff13ef6").openStream()) {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
             String jsonString = readAll(rd);
-            System.out.println(jsonString);
-            //String jsonString = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8"))); ; //assign your JSON String here
-            JSONObject obj = new JSONObject(jsonString);
-            System.out.println(obj);
-            //String pageName = obj.getJSONObject("pageInfo").getString("pageName");
-            back.add(getCurrentWeather(plz, country));
-            JSONArray dayName = obj.getJSONArray("dayOfWeek");
-            JSONArray tempMin = obj.getJSONArray("temperatureMin");
-            JSONArray tempMax = obj.getJSONArray("temperatureMax");
-            JSONArray nar = obj.getJSONArray("narrative");
+            JSONObject weatherData = new JSONObject(jsonString);
+            JSONArray dayName = weatherData.getJSONArray("dayOfWeek");
+            JSONArray tempMin = weatherData.getJSONArray("temperatureMin");
+            JSONArray tempMax = weatherData.getJSONArray("temperatureMax");
+            JSONArray nar = weatherData.getJSONArray("narrative");
             for (int i = 1; i < dayName.length(); i++) {
-                if (tempMax.get(i).equals(null))
-                    back.add(new Day(tempMin.getInt(i), 20, nar.getString(i)));
-                else
-                    back.add(new Day(tempMin.getInt(i), tempMax.getInt(i), nar.getString(i)));
+                back.add(new Day(tempMin.getInt(i), tempMax.getInt(i), nar.getString(i)));
             }
-           /* JSONObject day = (JSONObject) obj.getJSONArray("daypart").get(0);
-            System.out.println(day.length());
-            System.out.println(day);
-            JSONArray arr1 = day.getJSONArray("daypartName");
-            JSONArray nar1 = day.getJSONArray("narrative");
-            for (int i = 0; i < day.length(); i++) {
-                System.out.println(arr1.get(i) + " - " + nar1.get(i));
-            }
-
-            */
         } catch (Exception e) {
             System.out.println(e);
-        } finally {
-            is.close();
         }
         return back;
     }
 
-    protected static Day getCurrentWeather(String plz, String country) throws IOException {
-        Day back = null;
-        InputStream is = new URL("http://api.openweathermap.org/data/2.5/weather?zip=" + plz + "," + country + "&appid=6b5717bc865ffcb87230cfbcf6263078&units=metric&lang=de").openStream();
-        try {
+    protected static void getWeather(String plz, String country) {
+        try (InputStream is = new URL("http://api.openweathermap.org/data/2.5/forecast?zip=" + plz + "," + country + "&appid=6b5717bc865ffcb87230cfbcf6263078&units=metric&lang=de").openStream()) {
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
             String jsonString = readAll(rd);
             JSONObject weatherData = new JSONObject(jsonString);
+            System.out.println("-");
+            System.out.println(weatherData);
+            System.out.println(".");
+            JSONArray test = weatherData.getJSONArray("list");
+            System.out.println(test.length());
+            // JSONObject weather = weatherData.getJSONArray("weather").getJSONObject(0);
+            // JSONObject temp = weatherData.getJSONObject("main");
+        } catch (Exception e) {
+            System.err.println("Da lief etwas schief!");
+            e.printStackTrace();
+        }
+    }
+
+    protected static Day getCurrentWeather(String plz, String country) {
+        Day back = null;
+        try (InputStream is = new URL("http://api.openweathermap.org/data/2.5/weather?zip=" + plz + "," + country + "&appid=6b5717bc865ffcb87230cfbcf6263078&units=metric&lang=de").openStream()) {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            String jsonString = rd.readLine();
+            JSONObject weatherData = new JSONObject(jsonString);
             JSONObject weather = weatherData.getJSONArray("weather").getJSONObject(0);
             JSONObject temp = weatherData.getJSONObject("main");
-            ZonedDateTime zo = ZonedDateTime.ofInstant(Instant.ofEpochSecond(weatherData.getInt("dt")), ZoneId.systemDefault());
-            String day_name = zo.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.GERMAN);
             back = new Day(temp.getInt("temp_min"), temp.getInt("temp_max"), weather.getString("description"));
             back.setFeelsLike(temp.getInt("feels_like"));
             back.setCurrentTemp(temp.getInt("temp"));
         } catch (Exception e) {
             System.err.println("Da lief etwas schief!");
             e.printStackTrace();
-        } finally {
-            is.close();
         }
         return back;
     }

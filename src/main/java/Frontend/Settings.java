4fb88a01176e74be2c2696f4be295ca993d29502
hamidemutlu <hamidemtl@gmail.com>
2020-12-29 18:12:31 +0100

    Settings 2

--- src/main/java/Frontend/Settings.java
+++ src/main/java/Frontend/Settings.java
@@ -1,6 +1,24 @@
 package Frontend;
 
-public class Settings {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
 
+    public class Settings {
 
-}
+        @FXML
+        private MenuButton Temperature;
+
+        @FXML
+        private TextField Position;
+
+        @FXML
+        private Button Export;
+
+        @FXML
+        private ColorPicker Colourpick;
+
+    }
